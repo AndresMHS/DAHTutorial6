d93e9cf43ad0ee48f114b8136a2b9892e4ba5eef
Andres Haro <andres_h03@outlook.com>
2020-12-22 12:45:38 -0700

    V0.0.7

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Estudiante } from '../models/estudiante';
 import { EstudianteService } from '../services/estudiante.service';
+import { Router, NavigationExtras} from "@angular/router"
+
 
 @Component({
   selector: 'app-home',
@@ -11,7 +13,7 @@ export class HomePage {
 
   public students: Estudiante[];
 
-  constructor(private service:EstudianteService) {
+  constructor(private service:EstudianteService, private router: Router) {
     this.service.getStudents().subscribe(data => {
       this.students = data.map(e => {
         return {
@@ -25,6 +27,15 @@ export class HomePage {
   update(student:Estudiante,active:boolean){
     student.active=active;
     this.service.updateStudent(student,student.id)
+  }
+
+  detail(student: Estudiante){
+    let navext: NavigationExtras={
+      queryParams:{
+        special: JSON.stringify(student)
+      }
+    };
+    this.router.navigate(['/detail'],navext);
   }
 
 }
